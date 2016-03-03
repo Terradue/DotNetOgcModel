8e1227072cec634437d2c3f0181cede9d70e7b72
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-03-03 11:41:04 +0100

    nuget4mono.extensions added

--- Terradue.ServiceModel.Ogc/Properties/AssemblyInfo.cs
+++ Terradue.ServiceModel.Ogc/Properties/AssemblyInfo.cs
@@ -34,29 +34,20 @@
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
+using NuGet4Mono.Extensions;
 
 [assembly: AssemblyTitle("Terradue.ServiceModel.Ogc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Terradue.ServiceModel.Ogc is a library targeting .NET 4.0 to handle all OGC schemas serialization mainly in XML")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Terradue")]
 [assembly: AssemblyProduct("Terradue.ServiceModel.Ogc")]
 [assembly: AssemblyCopyright("Terradue")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyAuthors("Emmanuel Mathot")]
+[assembly: AssemblyIconUrl("")]
+[assembly: AssemblyProjectUrl("https://github.com/Terradue/DotNetOgcOmGml")]
+[assembly: AssemblyLicenseUrl("https://github.com/Terradue/DotNetOgcOmGml/blob/master/LICENSE")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
 
