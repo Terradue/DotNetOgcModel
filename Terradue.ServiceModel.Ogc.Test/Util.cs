afc31c66feee3ad270c539e33764f3f6ddecdb47
emmanuelmathot <emmanuel.mathot@gmail.com>
2019-06-10 21:59:39 +0200

    ready to build netstandard2.0

--- Terradue.ServiceModel.Ogc.Test/Util.cs
+++ Terradue.ServiceModel.Ogc.Test/Util.cs
@@ -20,25 +20,6 @@ namespace Terradue.ServiceModel.Ogc.Test
             TestBaseDir = Path.Combine(baseDir, "../../..");
         }
 
-        static public bool IsXmlEqual(XmlReader x1, XmlReader x2, XmlWriter xw,
-                                      bool IgnoreChildOrder, bool IgnoreComments, bool IgnorePI, bool IgnoreWhitespace,
-                                      bool IgnoreNamespaces, bool IgnorePrefixes, bool IgnoreXmlDecl, bool IgnoreDtd
-                                     )
-        {
-            XmlDiffOptions options = XmlDiffOptions.None;
-            if (IgnoreChildOrder) options |= XmlDiffOptions.IgnoreChildOrder;
-            if (IgnoreComments) options |= XmlDiffOptions.IgnoreComments;
-            if (IgnorePI) options |= XmlDiffOptions.IgnorePI;
-            if (IgnoreWhitespace) options |= XmlDiffOptions.IgnoreWhitespace;
-            if (IgnoreNamespaces) options |= XmlDiffOptions.IgnoreNamespaces;
-            if (IgnorePrefixes) options |= XmlDiffOptions.IgnorePrefixes;
-            if (IgnoreXmlDecl) options |= XmlDiffOptions.IgnoreXmlDecl;
-            if (IgnoreDtd) options |= XmlDiffOptions.IgnoreDtd;
-
-            XmlDiff xmlDiff = new XmlDiff(options);
-            bool bequal = xmlDiff.Compare(x1, x2, xw);
-            return bequal;
-        }
     }
 }
 
